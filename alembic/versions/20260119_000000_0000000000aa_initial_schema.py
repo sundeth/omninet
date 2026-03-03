3f2ee6d4790b3d610e87e9a57152b787ccf1676d
Sundeth <anderporto@gmail.com>
2026-03-03 10:09:50 -0300

    fix: add initial schema migration

--- alembic/versions/20260119_000000_0000000000aa_initial_schema.py
+++ alembic/versions/20260119_000000_0000000000aa_initial_schema.py
@@ -8,8 +8,8 @@ Create Date: 2026-01-19 00:00:00.000000
 
 from collections.abc import Sequence
 
-import sqlalchemy as sa
 from alembic import op
+import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
